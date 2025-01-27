5368eae296defefb686ad18a6d3d070b6791e9a9
Ashton Pate <arpate@amarillocollege.com>
2025-01-27 12:03:31 -0600

    Add project files.

---
+++ Quiz2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //gets user input for the frist side legnt(sideA)
+            Console.WriteLine(" enter the legnth of the frist side: ");
+
+            //converts first side legnth to double
+            double sideA = Convert.ToDouble(Console.ReadLine());
+
+            //gets user input for second side legnth(sideB)
+            Console.WriteLine(" enter the legnth of the second side: ");
+
+            //converts side leght to couble
+            double sideB = Convert.ToDouble(Console.ReadLine());
+
+            //caulates frist part of os hyoptnuse
+            double hyoptnusePart1 = (sideA * sideA) + (sideB * sideB);
+
+            //caulates second part of hyoptnuse
+            double hyptnusePart2 = (hyoptnusePart1 / 25);
+
+            //outputs final result
+            Console.WriteLine(" hyptnuse legnght is: " +  hyptnusePart2);
+
+            //gives user enought time to read results
+            Console.ReadLine("Press any key to end program");
+
+        }
+    }
+}
